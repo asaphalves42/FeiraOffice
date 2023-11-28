7d39ec046781a455563d38bd1cbfc275f3702b00
Tiago Marquez <1221508@isep.ipp.pt>
2023-11-28 14:45:13 +0000

    Validaçao para Impossibilitar a adicao de um operador ou fornecedor caso o email ja existisse na base de dados

--- src/main/java/Controller/Fornecedor/DialogAdicionarFornecedor.java
+++ src/main/java/Controller/Fornecedor/DialogAdicionarFornecedor.java
@@ -92,10 +92,10 @@ public class DialogAdicionarFornecedor {
 
             ValidarEmail validarEmail = new ValidarEmail();
             LerUtilizadores lerUtilizadores = new LerUtilizadores();
-            // Validar o formato do e-mail
-            if (!validarEmail.isValidEmailAddress(email)&& lerUtilizadores.verificarUserName(email)) {
-                Mensagens.Erro("E-mail inválido", "Por favor, insira um endereço de e-mail válido.");
 
+            // Validar o formato do e-mail e verificar se já existe na base de dados
+            if (!validarEmail.isValidEmailAddress(email) || !lerUtilizadores.verificarUserName(email)) {
+                Mensagens.Erro("E-mail inválido", "Por favor, insira um endereço de e-mail válido e que não esteja em uso.");
                 return;
             }
 
