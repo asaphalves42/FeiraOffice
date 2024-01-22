657c4dafbd31ad2f2b825140e2372e8305a7b328
Asaph Alves <1221473@isep.ipp.pt>
2024-01-22 21:00:12 +0000

    Pagina de conclusão de compra (inacabado)

---
+++ APILP3/Areas/Identity/Pages/Account/Manage/FinalizarCompra.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace APILP3.Areas.Identity.Pages.Account.Manage
+{
+    public class FinalizarCompraModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
