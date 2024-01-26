94af4a3127f935c3ca1a8184d12353661ac427c6
Asaph Alves <1221473@isep.ipp.pt>
2024-01-26 10:39:57 +0000

    mostrar produtos selecionados.

--- APILP3/Areas/Identity/Pages/Account/Manage/FinalizarCompra.cshtml.cs
+++ APILP3/Areas/Identity/Pages/Account/Manage/FinalizarCompra.cshtml.cs
@@ -1,12 +1,32 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using APILP3.Models;
+using Newtonsoft.Json;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace APILP3.Areas.Identity.Pages.Account.Manage
 {
     public class FinalizarCompraModel : PageModel
     {
-        public void OnGet()
+        private readonly ILogger<FinalizarCompraModel> _logger;
+
+        public FinalizarCompraModel(ILogger<FinalizarCompraModel> logger)
         {
+            _logger = logger;
         }
-    }
-}
+        public List<Product> SelectedProducts { get; set; } = new List<Product>();
+
+        public async Task<IActionResult> OnGet()
+        {
+                // Recuperar os produtos selecionados armazenados no localStorage
+                var selectedProductsJson = HttpContext.Request.Cookies["selectedProducts"];
+                 _logger.LogInformation(selectedProductsJson);
+
+             
+             return null;
+        }
+            
+       }
+  }
+    
+
